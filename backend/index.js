57518b4de609d78708bcacb62241a3fbec2c6a7c
Surafel Workayehu <surafelwork32@gmail.com>
2023-08-06 23:38:23 +0300

    add get transaction

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,34 @@
 const express=require('express');
-const app=express();
+const dotenv = require('dotenv');
+
 const cors=require('cors');
-
+const Transaction= require('./models/transaction');
+const { default: mongoose } = require('mongoose');
+const app=express();
+// Load configuration from a .env file which is found in the backend
+dotenv.config();
 app.use(cors());
-
 app.use(express.json())
 app.get('/api/test',(req,res)=>{
     res.json('test is working')
 });
 
-app.post('/api/transaction',(req,res)=>{
-    res.json(req.body);
+app.post('/api/transaction',async (req,res)=>{
+
+    await mongoose.connect(process.env.MONGO_URL)
+    try{
+    const {price,name,description,datetime}=req.body;
+    const transaction=await Transaction.create({price,name,description,datetime})
+    res.json(transaction);
+    }catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
+
+app.get('/api/transactions',async(req,res)=>{
+    await mongoose.connect(process.env.MONGO_URL);
+    const transactions=await Transaction.find();
+    res.json(transactions)
 })
 app.listen(4000);
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 
@@ -6,27 +6,53 @@ function App() {
   const[name,setName]=useState(''); 
   const[datetime,setDateTime]=useState('');
   const[description,setDescription]=useState('');
+  const [transactions,setTransactions]=useState([]);
+  useEffect(()=>{
+    getTransaction().then(setTransactions)
+  },[])
+
+  async function getTransaction(){
+    const url=process.env.REACT_APP_URL+'/transactions'
+    const response= await fetch(url);
+    return await response.json();
+  }
+
   function addNewTransaction(event){
     event.preventDefault();
     const url=process.env.REACT_APP_URL +'/transaction';
-    
+    const price=name.split(" ")[0];
     fetch(
       url,
       {
         method:'POST',
         headers:{'content-type':'application/json'},
-        body:JSON.stringify({name,description,datetime})
+        body:JSON.stringify(
+          { price,
+            name:name.substring(price.length+1),
+            description,
+            datetime,
+          })
       }
     ).then(response=>{
       response.json().then(json =>{
+        setName('');
+        setDateTime('');
+        setDescription('');
         console.log(json);
       })
     })
   }
+  let balance=0;
+  transactions.forEach((transaction) => {
+    balance = balance + transaction.price;
+  });
+  balance=balance.toFixed(2);
+  const fraction=balance.split(".")[1]
+  balance=balance.split('.')[0]
 
   return (
     <main>
-      <h1>400 <span>.00</span></h1>
+      <h1>{balance}<span>{fraction}</span></h1>
       <form onSubmit={addNewTransaction}>
         <div className='basics'>
         <input type="text"
@@ -47,39 +73,24 @@ function App() {
         <button type='submit'>Add transaction</button>
       </form>
       <div className='transactions'>
-        <div className='transaction'>
-          <div className='left'>
-            <div className='name'>Earphone</div>
-            <div className='description'>It was cheap</div>
+      {transactions.length > 0 &&
+      transactions.map((transaction) => (
+        <div className="transaction" key={transaction.id}>
+          <div className="left">
+            <div className="name">{transaction.name}</div>
+            <div className="description">{transaction.description}</div>
           </div>
-          <div className='right'>
-            <div className='price red'>-$300</div>
-            <div className='datetime'>2023-08-4 10:59</div>
-      
-          </div>
-        </div>
-        <div className='transaction'>
-          <div className='left'>
-            <div className='name'>Remote Job</div>
-            <div className='description'>It was cheap</div>
-          </div>
-          <div className='right'>
-            <div className='price green'>+$600</div>
-            <div className='datetime'>2023-08-4 10:59</div>
-      
+          <div className="right">
+            <div className={'price ' + (transaction.price < 0 ? 'red' : 'green')}>
+              {transaction.price}
+            </div>
+            <div className="datetime">2023-08-4 10:59</div>
           </div>
         </div>
-        <div className='transaction'>
-          <div className='left'>
-            <div className='name'>New Keyboard</div>
-            <div className='description'>It was cheap</div>
-          </div>
-          <div className='right'>
-            <div className='price red'>-$200</div>
-            <div className='datetime'>2023-08-4 10:59</div>
-      
-          </div>
-        </div>
+      ))
+    }
+        
+        
       </div>
     </main>
     );
